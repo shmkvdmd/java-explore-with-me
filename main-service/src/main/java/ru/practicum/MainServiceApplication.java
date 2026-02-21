4735e8c11c2013fc763669c0749f23e215e13577
Demid <demidka100@mail.ru>
2026-02-21 20:34:58 +0300

    fix: updated property

--- main-service/src/main/java/ru/practicum/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/MainServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatsServerApplication {
+public class MainServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(StatsServerApplication.class, args);
+        SpringApplication.run(MainServiceApplication.class, args);
     }
 }
