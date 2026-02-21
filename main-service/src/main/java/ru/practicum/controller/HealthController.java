b7fa139d0026144fc43f4a57b65b993ca117bf06
Demid <demidka100@mail.ru>
2026-02-21 20:09:41 +0300

    fix: add temp health controller

---
+++ main-service/src/main/java/ru/practicum/controller/HealthController.java
@@ -0,0 +1,14 @@
+package ru.practicum.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthController {
+    @GetMapping("/actuator/health")
+    public Map<String, String> health() {
+        return Map.of("status", "UP");
+    }
+}
